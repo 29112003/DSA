5c65288a74cb9beb2c41de9f755e7ab1f0ef836b
29112003 <thisiskeshavyadavv@gmail.com>
2024-11-21 15:48:08 +0530

    P26. 189. Rotate Array

--- src/Day7.java
+++ src/Day7.java
@@ -59,35 +59,93 @@ import java.util.Scanner;
 //    }
 //}
 //   P25. Left Rotation by K elements
+//public class Day7{
+//    public static void main(String[] args) {
+//        int [] arr = {1,2,3,4,5};
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        int k = sc.nextInt();
+//
+//        k = k % arr.length;
+//
+//        if(k != 0){
+//
+//        reverseString(arr , 0 , k -1);
+//        reverseString(arr , k , arr.length - 1);
+//        reverseString(arr , 0 , arr.length - 1);
+//
+//        }
+//
+//        System.out.println(Arrays.toString(arr));
+//    }
+//    public static void reverseString(int [] s , int left , int right) {
+//
+//        while(left <= right){
+//            int temp = s[left];
+//            s[left] = s[right];
+//            s[right] = temp;
+//            left++;
+//            right--;
+//        }
+//    }
+//
+//}
+
+//P26. 189. Rotate Array
+
 public class Day7{
     public static void main(String[] args) {
-        int [] arr = {1,2,3,4,5};
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("enter the size of array");
+        int size = sc.nextInt();
+
+        System.out.println("enter the kth from right");
         int k = sc.nextInt();
 
-        k = k % arr.length;
+        int [] nums = new int[size];
+
+        System.out.println("enter the elements in the array");
+
+        for(int i = 0 ; i < size ; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Original array ");
+        System.out.println(Arrays.toString(nums));
+
+        System.out.println("Rotated array is : ");
+
+        rotate(nums , k);
+
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
 
         if(k != 0){
 
-        reverseString(arr , 0 , k -1);
-        reverseString(arr , k , arr.length - 1);
-        reverseString(arr , 0 , arr.length - 1);
+            reverseArray(nums , nums.length - k   , nums.length - 1);
+            reverseArray(nums , 0 , nums.length - k - 1);
+            reverseArray(nums , 0 , nums.length - 1);
 
         }
 
-        System.out.println(Arrays.toString(arr));
     }
-    public static void reverseString(int [] s , int left , int right) {
-
+    public static void reverseArray(int [] arr , int left , int right){
         while(left <= right){
-            int temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
             left++;
             right--;
         }
     }
+
+//    Overall Complexity:
+//    Time Complexity: O(n), where n is the number of elements in the array.
+//    Space Complexity: O(1), since the algorithm performs the rotations in-place without requiring extra space.
 
 }
