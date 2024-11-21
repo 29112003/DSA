a1a78564ee9316eeeffa63357667037705ea5048
29112003 <thisiskeshavyadavv@gmail.com>
2024-11-21 15:00:09 +0530

    p 24 leetcode 344

--- src/Day7.java
+++ src/Day7.java
@@ -1,39 +1,60 @@
+import java.util.Arrays;
 import java.util.Scanner;
 //p 23
-public class Day7 {
+//public class Day7 {
+//    public static void main(String[] args) {
+//
+//        int [] arr = {1,2,342,34,423,23,-324};
+//
+//        int Largest = Integer.MIN_VALUE;
+//
+//        int secondLargest = Integer.MIN_VALUE;
+//
+//        for(int i  = 0 ; i < arr.length ; i++){
+//
+//            if(Largest  < arr[i]){
+//
+//                secondLargest = Largest;
+//                Largest = arr[i];
+//
+//            }else if(Largest != arr[i] && secondLargest < arr[i]){
+//
+//                secondLargest = arr[i];
+//
+//            }
+//
+//
+//        }
+//        if(secondLargest == Integer.MIN_VALUE){
+//
+//            System.out.print(Largest + " and the secondLargest is not available in array");
+//
+//        }else {
+//
+//            System.out.print(Largest + " and the secondLargest is : " + secondLargest);
+//
+//        }
+//
+//    }
+//
+//}
+// p 24 leetcode : 344
+public class Day7{
     public static void main(String[] args) {
-
-        int [] arr = {1,2,342,34,423,23,-324};
-
-        int Largest = Integer.MIN_VALUE;
-
-        int secondLargest = Integer.MIN_VALUE;
-
-        for(int i  = 0 ; i < arr.length ; i++){
-
-            if(Largest  < arr[i]){
-
-                secondLargest = Largest;
-                Largest = arr[i];
-
-            }else if(Largest != arr[i] && secondLargest < arr[i]){
-
-                secondLargest = arr[i];
-
-            }
-
-
-        }
-        if(secondLargest == Integer.MIN_VALUE){
-
-            System.out.print(Largest + " and the secondLargest is not available in array");
-
-        }else {
-
-            System.out.print(Largest + " and the secondLargest is : " + secondLargest);
-
-        }
-
+        char [] s =new char[] {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
     }
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
 
+        while(left <= right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
 }
